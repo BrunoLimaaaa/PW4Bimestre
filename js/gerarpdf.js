952453f4e882650c9fd6b2220971c879fe18b8d6
BrunoLimaaaa <162579597+BrunoLimaaaa@users.noreply.github.com>
2024-11-17 20:10:50 -0300

    Add files via upload

---
+++ js/gerarpdf.js
@@ -0,0 +1,57 @@
+const btnGenerate = document.getElementById("generate-pdf");
+
+function savePdf() {
+    const content = document.querySelector("#content");
+
+
+
+    const style = document.createElement("style");
+    style.innerHTML = `
+        @media (max-width:768px) {
+            .section,
+            .section_ambiente,
+            .section_partida {
+                flex-direction: column;
+                padding: 1.5rem;
+            }
+
+            .section_ambiente img,
+            .section_partida img {
+                max-width: 250px;
+                height: auto;
+            }
+
+            .section p,
+            .section_ambiente p,
+            .section_partida p {
+                font-size: 12px;
+            }
+
+            .header p {
+                font-size: 10px;
+                text-align: center;
+            }
+
+            .logo {
+                max-width: 20px;
+            }
+        }
+    `;
+    document.head.appendChild(style);
+
+    const options = {
+        margin: [-8, -8, -8, -8],
+        filename: "arquivo.pdf",
+        html2canvas: { scale: 3, useCORS: true },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }
+    };
+
+
+
+    html2pdf().set(options).from(content).save();
+}
+
+btnGenerate.addEventListener("click", () => {
+    alert('Baixando PDF');
+    savePdf();
+});
